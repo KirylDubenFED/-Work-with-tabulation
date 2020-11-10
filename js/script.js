3014a909a0f60ec15dec17d85330ff5999199b59
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2020-11-11 02:54:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('.tab');
+const tabPanels = tabs.querySelectorAll('.tabpanel');
+
+function tabClick(event) {
+  tabPanels.forEach(function(panel) {
+    panel.hidden = true;
+  });
+  const {id} = event.currentTarget;
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  tabPanel.hidden = false;
+}
+tabButtons.forEach(button => button.addEventListener('click', tabClick));
