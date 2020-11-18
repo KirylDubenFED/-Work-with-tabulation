426d3ee9d6c894d9896d8da1a7269015fde57fcc
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2020-11-19 01:10:12 +0300

    fix background

---
+++ keys.js
@@ -0,0 +1,16 @@
+let users = {
+    ivanov: '19',
+    petrov: '17',
+    sidorov: '21'
+ }
+
+ let keys = Object.keys(users);
+ console.log(keys);
+ 
+
+ 
+
+ 
+ let young = []
+ arr.forEach(item)
+
